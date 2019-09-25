85d3840a0485f459bf14be9d73839aa4ebb10482
ijayden-lung <ijayden.lung@gmail.com>
2019-09-25 19:55:40 +0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # n-Queen-Problem
+
+For CS220 Data Analytics Assignment 1: Uninformed and Informed Search
+@Yongkang Long 171022
+
+## How to use
+pytyon3 count_solution.py
--- count_solution.py
+++ count_solution.py
@@ -4,6 +4,7 @@ import numpy as np
 N = [4,6,8,10]
 
 def attack_block(state,row,col):
+	#mark the attach block, no queen will be placed.
 	new_state = np.copy(state)
 	availBlock = []
 	for i in range(n):
@@ -13,12 +14,12 @@ def attack_block(state,row,col):
 			elif(state[i,j]==0 and j==col+1):
 				availBlock.append(i)
 	if(availBlock):
-		naive_algorithm(new_state,availBlock,col+1)
+		check_row(new_state,availBlock,col+1)
 	#else:
 	#	print("Faluire")
 
 
-def naive_algorithm(state,availBlock,col):
+def check_row(state,availBlock,col):
 	global success_count
 	for row in availBlock:
 		if(col == n-1):
@@ -32,6 +33,6 @@ if __name__ == '__main__':
 		success_count= 0
 		possible = [i for i in range(n)]
 		state = np.zeros([n,n])
-		naive_algorithm(state,possible,0)
+		check_row(state,possible,0)
 		print('number of solutions for ',n,'queens is: ',success_count)
 
